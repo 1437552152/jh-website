e9619a26d5540eec5605b255b59944fcae71b101
1437552152 <1437552152@qq.com>
2018-12-10 00:37:44 +0800

    提交

--- config.js
+++ config.js
@@ -1,1 +1,4 @@
-var  websiteURL="http://127.0.0.1:8888/user"
+axios.defaults.timeout=180000;
+axios.defaults.headers['Content-Type']='application/x-www-form-urlencoded;charset=UTF-8';
+axios.defaults.baseURL="http://47.107.180.202:8080/user";
+
